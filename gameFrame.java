ec6d7b27447b09bc2b7f6b040d3d25769141a5c1
santoshkumar2001 <59115325+santoshkumar2001@users.noreply.github.com>
2023-07-25 21:22:26 -0500

    Add files via upload

---
+++ gameFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class gameFrame extends JFrame {
+	gameFrame(){
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);;
+	}
+}
